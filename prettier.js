c37f813f0bb5f52bf673a81ce11e2d775c0aa84e
Christian Garrovillo <6688713+cgarrovillo@users.noreply.github.com>
2022-02-07 15:08:03 -0700

    Prettier configs

---
+++ prettier.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
+};
