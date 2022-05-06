a9cd399ae25d0387422594b6623a36504a8bedbc
Christian Garrovillo <6688713+cgarrovillo@users.noreply.github.com>
2022-05-06 14:57:38 -0600

    Folder restructures. exported API structures.

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * ========================================================
+ * ========================= API ==========================
+ * ========================================================
+ */
+
+/**
+ * '@goodlawyer/eslint-config/'
+ */
+module.exports = require("./stable/index");
+
+/**
+ * '@goodlawyer/eslint-config/stable/'
+ */
+module.exports.stable = require("./stable/index");
+
+/**
+ * '@goodlawyer/eslint-config/next/'
+ */
+// module.exports.next = require("./next/index");
--- stable/index.js
+++ stable/index.js
@@ -1,1 +1,25 @@
-module.exports = require("./base");
+/**
+ * ========================================================
+ * =============== STABLE Release schedule ================
+ * ========================================================
+ */
+
+/**
+ * '@goodlawyer/eslint-config/stable'
+ */
+module.exports = require("./eslint/base");
+
+/**
+ * '@goodlawyer/eslint-config/stable/backend'
+ */
+module.exports.backend = require("./eslint/backend");
+
+/**
+ * '@goodlawyer/eslint-config/stable/frontend'
+ */
+module.exports.frontend = require("./eslint/frontend");
+
+/**
+ * '@goodlawyer/eslint-config/stable/prettier'
+ */
+module.exports.prettier = require("./prettier/prettier.config.cjs");
