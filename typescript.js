21ebfd3e52e8560f6c625e7083f601ad749275e3
Christian Garrovillo <6688713+cgarrovillo@users.noreply.github.com>
2022-05-11 11:31:10 -0600

    Adds import plugin config for TS

--- typescript.js
+++ typescript.js
@@ -4,10 +4,14 @@
  * ========================================================
  */
 
- module.exports = {
-	extends: [
-		"./base.js",
-		"plugin:@typescript-eslint/recommended"
-	],
-	parser: "@typescript-eslint/parser"
+module.exports = {
+	extends: ["./base.js", "plugin:@typescript-eslint/recommended"],
+	parser: "@typescript-eslint/parser",
+	settings: {
+		"import/resolver": {
+			node: {
+				extensions: [".js", ".jsx", ".ts", ".tsx"],
+			},
+		},
+	},
 };
