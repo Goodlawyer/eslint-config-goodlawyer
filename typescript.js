5adc0109c2e7da1a2ccf4cd99c74703cb0ea39b7
Christian Garrovillo <6688713+cgarrovillo@users.noreply.github.com>
2022-08-21 13:30:31 -0600

    Major bump. Adds support for module aliases

--- typescript.js
+++ typescript.js
@@ -6,14 +6,23 @@
 
 module.exports = {
 	extends: ["./base.js", "plugin:@typescript-eslint/recommended"],
-	parser: "@typescript-eslint/parser",
 	settings: {
 		"import/resolver": {
 			node: {
 				extensions: [".js", ".jsx", ".ts", ".tsx"],
 			},
+			// Support for tsconfig.json baseUrl
+			typescript: {},
 		},
 	},
+	parser: "@typescript-eslint/parser",
+	// Support for tsconfig.json baseUrl
+	parserOptions: {
+		project: "./tsconfig.json",
+		tsconfigRootDir: "./",
+	},
+	// Support for tsconfig.json baseUrl
+	plugins: ["@typescript-eslint", "import"],
 	rules: {
 		"no-unused-vars": "off",
 		"@typescript-eslint/no-unused-vars": "error",
